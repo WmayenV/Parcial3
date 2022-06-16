c5ba77ead0756318dcadfcda5850d8cc9368c904
William Mayen <willysomv101@gmail.com>
2022-06-16 11:30:27 -0600

    imicioMain

---
+++ PrototipoP1EF2022.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <cstdlib>
+#include <iomanip>
+#include <windows.h>
+#include <string>
+#include <stdio.h>
+#include <time.h>
+#include<conio.h>
+#include<math.h>
+
+using namespace std;
